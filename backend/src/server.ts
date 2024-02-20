5dc2e91d0057c412ef21a5a0a687e75ca383a223
jhondharkyson <jhondharkyson@gmail.com>
2024-02-20 20:12:58 -0300

    Testes do sistema

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,9 +4,13 @@ import cors from 'cors'
 
 import { router } from './routes';
 
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const port = process.env.PORT || 3001;
 
 app.use(router);
 
