181497b1729cb7ddf3e197d0bd137ee7dd371619
jhondharkyson <jhondharkyson@gmail.com>
2024-02-26 13:39:49 -0300

    Finalizando UI tela de Login

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,12 +1,96 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Image } from "react-native";
+import * as Animatable from 'react-native-animatable'
 
 export default function SignIn(){
     return(
 
-        <View>
-            <Text>Tela de login</Text>
+        <View style={styles.container} >
+            <Animatable.View animation='fadeInLeft' delay={500} style={styles.containerHeader}>
+                <Text style={styles.message}>Bem vindo(a)!</Text>
+            </Animatable.View>
+
+            <Animatable.View animation='fadeInUp' style={styles.containerForm}>
+
+                <Image
+                    style={styles.logo}
+                    source={require('../../assets/logosgcp.png')}
+                />
+
+                <Text style={styles.title}>Email</Text>
+                <TextInput
+                    placeholder="Digite seu email"
+                    style={styles.input}
+                />
+
+                <Text style={styles.title}>Senha</Text>
+                <TextInput
+                    placeholder="Digite sua senha"
+                    style={styles.input}
+                />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Acessar</Text>
+                </TouchableOpacity>
+
+            </Animatable.View>
         </View>
 
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#3FBAC2'
+    },
+    containerHeader:{
+        marginTop: '14%',
+        marginBottom: '8%',
+        paddingStart: '5%'
+    },
+    message:{
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#f1f1f1'
+    },
+    containerForm:{
+        backgroundColor: '#f1f1f1',
+        flex: 1,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%',
+        paddingTop: '10%'
+    },
+    logo:{
+        marginBottom: 18,
+        marginLeft: '22%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title:{
+        fontSize: 20,
+        marginTop: 28
+    },
+    input:{
+        borderBottomWidth: 1,
+        height: 40,
+        marginBottom: 12,
+        fontSize: 16
+    },
+    button:{
+        backgroundColor: '#3FBAC2',
+        width: '100%',
+        borderRadius: 4,
+        paddingVertical: 8,
+        marginTop: 14,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText:{
+        color: '#f1f1f1',
+        fontSize: 18,
+        alignSelf: 'center'
+    }
+})
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ActivityIndicator } from "react-native";
 
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
@@ -8,6 +8,22 @@ function Routes(){
 
     const isAuthenticated = false;
     const loading = false;
+
+    if(loading){
+        return(
+            <View 
+                style={{ 
+                    flex:1, 
+                    backgroundColor: '#f1f1f1', 
+                    justifyContent:'center', 
+                    alignItems:'center'
+                }} 
+            >
+                <ActivityIndicator size={60} color='#3FBAC2' />
+                
+            </View>
+        )
+    }
 
     return(
         isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
